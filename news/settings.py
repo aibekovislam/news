022b2fc67c183225265c87257d9bc7c8498d331f
aibekovislam <islamxdaamduu@gmail.com>
2022-02-18 16:20:29 +0300

    ad

--- news/settings.py
+++ news/settings.py
@@ -86,14 +86,14 @@ DATABASES = {
         'PASSWORD': 'islam2005',
         'HOST': 'aibekovislam.mysql.pythonanywhere-services.com',
         'PORT': '3306'
-
+    }
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [ 
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
