aec243ef70e85ac2b51f986a046f14d3bbba94cb
aibekovislam <islamxdaamduu@gmail.com>
2022-02-20 10:42:03 +0300

    static settings.py

--- news/settings.py
+++ news/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+
+
+STATIC_ROOT = "/home/aibekovislam/news/core/static"
 
 
 
