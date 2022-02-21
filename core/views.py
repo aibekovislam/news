6cf4a2b02be4022cb8fbf31a00797b5f7a02d537
aibekovislam <islamxdaamduu@gmail.com>
2022-02-21 16:42:10 +0300

    modified project

--- core/views.py
+++ core/views.py
@@ -12,7 +12,7 @@ def index(request):
 def articles(request):
     articles = Article.objects.all()
     heading = HeadingArticle.objects.all()
-    paginator = Paginator(articles, per_page=17)
+    paginator = Paginator(articles, per_page=20)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
     context = {
