de799f9a3c4ba9745c6769096326e5ea59680223
aibekovislam <islamxdaamduu@gmail.com>
2022-02-28 09:08:39 +0300

    gs

--- news/settings.py
+++ news/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "'django.contrib.sitemaps'",
-    "'django.contrib.sites'",
+    #'django.contrib.sitemaps',
+    #'django.contrib.sites',
     'core'
 ]
 
