f2e666d17be7bd66534f5c11358c6cff5d834b18
aibekovislam <islamxdaamduu@gmail.com>
2022-03-01 19:47:56 +0300

    xml

--- core/sitemap.py
+++ core/sitemap.py
@@ -1,12 +1,12 @@
 from django.contrib.sitemaps import Sitemap
-from django.shortcuts import reverse
 from .models import Article
 
-
 class AriclesSitemap(Sitemap):
-    
-    def item(self):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
         return Article.objects.all()
 
-    def location(self, item):
-        return reverse(item)
+    def lastmod(self, obj):
+        return obj.created_at
