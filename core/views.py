2711335d9ca9cbb4f7bbbfeb345bbb011eba5cb5
aibekovislam <islamxdaamduu@gmail.com>
2022-03-06 08:52:55 +0300

    modified paginator

--- core/views.py
+++ core/views.py
@@ -14,7 +14,7 @@ def articles(request):
     articles = Article.objects.all()
     articles = Article.objects.order_by('-created_at')
     heading = HeadingArticle.objects.all()
-    paginator = Paginator(articles, per_page=60)
+    paginator = Paginator(articles, per_page=50)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
     context = {
