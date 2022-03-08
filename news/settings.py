26f10ea4597ca87b56b10e849cd7cbefef803934
aibekovislam <islamxdaamduu@gmail.com>
2022-03-08 15:41:23 +0300

    modified

--- news/settings.py
+++ news/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,6 +26,7 @@ SECRET_KEY = '70-pxvs!io_e4dbasy78x^!(i=rpd52&gj4b#+tq7kg*^pzlgs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = os.environ.get("DEBUG", False)
 #DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
