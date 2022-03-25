3b9c504cdfa6780839bd3af45e87b54903178d35
aibekovislam <islamxdaamduu@gmail.com>
2022-03-25 07:32:02 +0300

    Ad

--- core/views.py
+++ core/views.py
@@ -16,7 +16,7 @@ def articles(request):
     articles = Article.objects.order_by('-created_at')
     obj = Carousel.objects.all()
     heading = HeadingArticle.objects.all()
-    paginator = Paginator(articles, per_page=100)
+    paginator = Paginator(articles, per_page=80)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
     context = {
