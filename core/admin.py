ab438903fb5b492566dd1f56197937e861fc691d
aibekovislam <islamxdaamduu@gmail.com>
2022-04-07 12:29:05 +0300

    fdaf

--- core/admin.py
+++ core/admin.py
@@ -12,7 +12,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active', 'updated_at', 'heading')
     list_editable = ('is_active', 'heading')
     ordering = ['-created_at']
-    list_filter = ['is_active', 'updated_at', 'main_article', 'center_article', 'right_panel_article', 'text_article', 'slug', 'pagination_article']
+    list_filter = ['is_active', 'updated_at', 'main_article', 'center_article', 'right_panel_article', 'text_article', 'pagination_article']
     search_fields = ['title', 'text']
     prepopulated_fields = {'slug': ("title",)}
     fields = ('title', 'text', 'picture', 'created_at', 'updated_at', 'main_article', 'center_article', 'right_panel_article', 'text_article',  'pagination_article', 'heading', 'slug', 'metades')
